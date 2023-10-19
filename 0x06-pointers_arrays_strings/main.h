3692420d1fca601b804a42f500194fe6db035703
Luther63 <Murangi516@gmail.com>
2023-10-19 16:39:30 -0700

    task 11

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -9,8 +9,10 @@ int _strcmp(char *s1, char *s2);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 char *rot13(char *);
 void print_number(int n);
 int _putchar(char c);
+
 
 #endif
