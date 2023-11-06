ce351f681ee97c443e0a4ed5b7dbc00e702ca685
Luther63 <Murangi516@gmail.com>
2023-11-06 08:55:49 -0800

    poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - this defines the main function
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: ownwe of the dog
+ */
+
 struct dog
 {
 	char *name;
